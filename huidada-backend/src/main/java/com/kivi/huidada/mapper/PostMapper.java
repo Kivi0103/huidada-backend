19c6a831e7d252b56c756db62565d81df9b08ad6
Kivi0313 <1693645338@qq.com>
2024-06-28 20:20:37 +0800

    项目初始化

---
+++ huidada-backend/src/main/java/com/kivi/huidada/mapper/PostMapper.java
@@ -0,0 +1,15 @@
+package com.kivi.huidada.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.kivi.huidada.model.entity.Post;
+
+/**
+ * 帖子数据库操作
+ */
+public interface PostMapper extends BaseMapper<Post> {
+
+}
+
+
+
+
---
+++ huidada-backend/src/main/java/com/kivi/huidada/mapper/UserMapper.java
@@ -0,0 +1,15 @@
+package com.kivi.huidada.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.kivi.huidada.model.entity.User;
+
+/**
+ * 用户数据库操作
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
